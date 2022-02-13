cf06299f393129479fd833a438f88a3ed4dc2e79
Serafim Nenarokov <serafim221@gmail.com>
2022-02-13 21:22:23 +0100

    Add suspicious mutation detection script

---
+++ find_suspicious_mutations.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# @Author  : Serafim Nenarokov (serafim.nenarokov@gmail.com)
+
+import os
+import argparse
+import re
+import json
+
+from tqdm import tqdm
+
+
+def parse_arguments():
+    usage = "parse_nextclade_report.py"
+    description = """
+    Parse nextclade report in COG json format and provide a list of suspicious mutations.
+    """
+    description = re.sub(r"(?<=\w)\n(?=\w)", "\n", description)
+    formatter = argparse.RawTextHelpFormatter
+
+    parser = argparse.ArgumentParser(usage,
+                                     description=description,
+                                     formatter_class=formatter)
+
+    out_path_help_str = """Path to the output file
+    (report.json file in nextclade output path will be created if not specified)"""
+
+    parser.add_argument("-i", "--report_path",
+                        required=True,
+                        help="Report in COG json format.")
+    parser.add_argument('-o', '--output_path',
+                        required=True,
+                        help="Json file with a list of suspicious mutations")
+    return parser.parse_args()
+
+MUTATION_REGEX = re.compile(
+    '(?P<gene_name>\w+):(?P<source_aa>[a-zA-Z\-\*])(?P<index>\d+)(?P<target_aa>[a-zA-Z\-\*])')
+
+def main():
+    arguments = parse_arguments()
+
+    with open(arguments.report_path) as f:
+        report = json.load(f)
+
+    by_seq_report = {}
+    # {'seq_id': {
+    #   'gene_id': { site_id: {
+    #                   code: "MUT_CODE",
+    #                   type: "DEL/SUB"}, ...},
+    #    ...},
+    # ...}
+    for mut_code, data in report.items():
+        mut_parsed = re.match(MUTATION_REGEX, mut_code)
+
+        if not mut_parsed:
+            raise(f'Mutation `{mut_code}` cannot be parsed!')
+
+        site_id = int(mut_parsed.group('index'))
+        gene_name = mut_parsed.group('gene_name')
+
+        if mut_parsed.group('target_aa') == '-':
+            mut_type = 'DEL'
+        else:
+            mut_type = 'SUB'
+
+        for seq_id in data['seq_ids']:
+            if seq_id not in by_seq_report:
+                by_seq_report[seq_id] = {}
+
+            if gene_name not in by_seq_report[seq_id]:
+                by_seq_report[seq_id][gene_name] = {}
+
+            by_seq_report[seq_id][gene_name][site_id] = {
+                'code': mut_code, 'type': mut_type}
+
+    susp_mutations = {}
+    # {'mut_code': {'cnt': N, 'seq_ids': [...]}, ...}
+    for seq_id, data in tqdm(by_seq_report.items()):
+        for gene_name, mutations in data.items():
+            site_ids = sorted(list(mutations.keys()))
+
+            del_found = False
+            last_id = None
+            for site_id in site_ids:
+                mut_type = mutations[site_id]['type']
+
+                # If first or new cluster of mutations
+                if last_id == None or site_id != last_id+1:
+                    del_found = mut_type == 'DEL'
+
+                elif mut_type == 'DEL':
+                    del_found = True
+
+                elif mut_type == 'SUB' and del_found:
+                    code = mutations[site_id]['code']
+
+                    if code not in susp_mutations:
+                        susp_mutations[code] = {'cnt': 0, 'seq_ids': []}
+
+                    susp_mutations[code]['seq_ids'].append(seq_id)
+                    susp_mutations[code]['cnt'] += 1
+
+                last_id = site_id
+
+    with open(arguments.output_path, 'w') as out_f:
+        json.dump(susp_mutations, out_f, indent=4, sort_keys=True)
+
+    print(f'Finished. {len(susp_mutations)} mutations found.')
+
+
+if __name__ == '__main__':
+    main()
